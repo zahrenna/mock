bce7d8598fcb635bfb7cc14a48e0da7995d49fdc
zahrenna <49585487+zahrenna@users.noreply.github.com>
2020-07-19 12:06:39 +0800

    Create shoe.js

---
+++ shoe.js
@@ -0,0 +1,12 @@
+[
+  {
+    "userId": 1,
+    "id": 2,
+    "productName": "Blouse",
+    "productPrice": "RM 20",
+    "productDescription": "Pakaian terhebat anda",
+    "productURL" : "https://raw.githubusercontent.com/zahrenna/mock/master/paule-ka_19H180H22_417_5.jpg",
+    "productCategory" : "APPAREL",
+    "contactNumber" : "https://wa.me/60193848848"
+  }
+]

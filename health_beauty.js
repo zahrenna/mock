f0451aa4da7949f5eac1a20fb5f545e1a389fc49
zahrenna <49585487+zahrenna@users.noreply.github.com>
2020-07-19 15:29:44 +0800

    Update health_beauty.js

--- health_beauty.js
+++ health_beauty.js
@@ -2,11 +2,21 @@
   {
     "userId": 1,
     "id": 2,
-    "productName": "Blouse",
-    "productPrice": "RM 20",
-    "productDescription": "Pakaian terhebat anda",
-    "productURL" : "https://raw.githubusercontent.com/zahrenna/mock/master/paule-ka_19H180H22_417_5.jpg",
-    "productCategory" : "APPAREL",
+    "productName": "Hada Labo Super Hyaluronic Acid Hydrating Face Wash (100g)",
+    "productPrice": "RM24.40",
+    "productDescription": "Contains advanced skin hydrating ingredient - Super Hyaluronic Acid, with twice the moisture retention capacity of Hyaluronic Acid (1gm of Hyaluronic Acid is able to hold up 6 liters of water). Replenishes essential moisture to visibly improve skin texture and prevent dryness so skin stays soft, hydrated and supple.",
+    "productURL" : "https://raw.githubusercontent.com/zahrenna/mock/master/beauty%202.jpeg",
+    "productCategory" : "BEAUTY",
+    "contactNumber" : "https://wa.me/60193848848"
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "productName": " 🔥🔥 TANAKO LIPSTICK 😍",
+    "productPrice": "RM13.50",
+    "productDescription": "🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑 🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑 🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑🍑 🍑🍑🍑🍑",
+    "productURL" : "https://raw.githubusercontent.com/zahrenna/mock/master/beauty%20%202.jpeg",
+    "productCategory" : "BEAUTY",
     "contactNumber" : "https://wa.me/60193848848"
   }
 ]

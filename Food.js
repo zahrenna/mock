cc922344717cc5e8cfdc2ca7caa4aa2597395391
zahrenna <49585487+zahrenna@users.noreply.github.com>
2020-07-19 11:33:55 +0800

    Update and rename db2.js to Food.js

---
+++ Food.js
@@ -0,0 +1,12 @@
+[
+  {
+    "userId": 1,
+    "id": 1,
+    "productName": "Pizza Hut",
+    "productPrice": "RM 2",
+    "productDescription": "Delicious hingga menjilat jari",
+    "productURL" : "https://raw.githubusercontent.com/zahrenna/mock/master/pizza-hut.jpg",
+    "productCategory" : "FOOD",
+    "contactNumber" : "https://wa.me/60178988603"
+  }
+]

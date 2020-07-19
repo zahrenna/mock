6f7c2f7fc2ea3faa4a0a2d82ca75057bc136373c
zahrenna <49585487+zahrenna@users.noreply.github.com>
2020-07-19 12:03:19 +0800

    Update Clothing.js

--- Clothing.js
+++ Clothing.js
@@ -7,4 +7,4 @@
     "productURL" : "https://raw.githubusercontent.com/zahrenna/mock/master/paule-ka_19H180H22_417_5.jpg",
     "productCategory" : "APPAREL",
     "contactNumber" : "https://wa.me/60193848848"
-  },]
+  }]

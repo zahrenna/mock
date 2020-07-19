2eac63f8765bfb89e9fc942c7481d4ee57ff89d8
zahrenna <49585487+zahrenna@users.noreply.github.com>
2020-07-19 15:28:55 +0800

    Update shoe.js

--- shoe.js
+++ shoe.js
@@ -2,11 +2,21 @@
   {
     "userId": 1,
     "id": 2,
-    "productName": "Blouse",
-    "productPrice": "RM 20",
-    "productDescription": "Pakaian terhebat anda",
-    "productURL" : "https://raw.githubusercontent.com/zahrenna/mock/master/paule-ka_19H180H22_417_5.jpg",
-    "productCategory" : "APPAREL",
-    "contactNumber" : "https://wa.me/60193848848"
+    "productName": "Vans Era Mismatch",
+    "productPrice": "RM 220",
+    "productDescription": " UK6 / US7 \n Condition 9/10",
+    "productURL" : "https://raw.githubusercontent.com/zahrenna/mock/master/shoe%201.jpeg",
+    "productCategory" : "SHOE",
+    "contactNumber" : "https://wa.me/60142980523"
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "productName": "Women's Heel ( size 35-40 )",
+    "productPrice": "RM32",
+    "productDescription": " Code : PYX8165 \n Color : Black \n Material : Silk \n Heel Height : 6.0 cm \n Cutting : Small Cutting( size 35 - 40) \n *Small Cutting: Take 1(one) size BIGGER from your feet measurement",
+    "productURL" : "https://raw.githubusercontent.com/zahrenna/mock/master/shoe%202.jpeg",
+    "productCategory" : "SHOE",
+    "contactNumber" : "https://wa.me/60142980523"
   }
 ]

7567a945dd33a7ec522cdc656db4cbafea0aa7a7
zahrenna <49585487+zahrenna@users.noreply.github.com>
2020-07-19 12:10:32 +0800

    Create service.js

---
+++ service.js
@@ -0,0 +1,12 @@
+[
+  {
+    "userId": 1,
+    "id": 4,
+    "productName": "Delivery",
+    "productPrice": "RM 5",
+    "productDescription": "Ayuhlah kita mendeliver muheheheh",
+    "productURL" : "https://raw.githubusercontent.com/zahrenna/mock/master/blgo2.jpg",
+    "productCategory" : "SERVICES",
+    "contactNumber" : "https://wa.me/60178988603"
+  }
+]

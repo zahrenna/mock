3ed48b668acd821d1f2e2e228e752138e734c004
zahrenna <49585487+zahrenna@users.noreply.github.com>
2020-07-19 12:12:33 +0800

    Create computer_accessories.js

---
+++ computer_accessories.js
@@ -0,0 +1,12 @@
+[
+  {
+    "userId": 1,
+    "id": 3,
+    "productName": "USB wire",
+    "productPrice": "RM 10",
+    "productDescription": "Wire kehidupan anda hihihihi",
+    "productURL" : "https://raw.githubusercontent.com/zahrenna/mock/master/71MF3XyeugL._AC_SL1500_.jpg",
+    "productCategory" : "COMPUTER & ACCESORIES",
+    "contactNumber" : "https://wa.me/60193848848"
+  }
+]

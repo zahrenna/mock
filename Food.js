b16ce6a03f9f29913b526d62740225ca42af0b53
zahrenna <49585487+zahrenna@users.noreply.github.com>
2020-07-19 12:40:45 +0800

    Update Food.js

--- Food.js
+++ Food.js
@@ -4,9 +4,39 @@
     "id": 1,
     "productName": "Pizza Hut",
     "productPrice": "RM 2",
-    "productDescription": "Delicious hingga menjilat jari",
+    "productDescription": " Delicious hingga menjilat jari",
     "productURL" : "https://raw.githubusercontent.com/zahrenna/mock/master/pizza-hut.jpg",
     "productCategory" : "FOOD",
     "contactNumber" : "https://wa.me/60178988603"
+  },
+  {
+    "userId": 1,
+    "id": 1,
+    "productName": " Moist chocolate cake",
+    "productPrice": "RM 15",
+    "productDescription": " Ready stock Moist chocolate cake utk esok (Ahad tengahari). Boleh share dgn roomates korang🤤! \n Berat: 300g-350g \n 🚛 Available utk cod Mahallah Hafsa, dan around UIA (free Cod kalau pada tgahri esok) ",
+    "productURL" : "https://raw.githubusercontent.com/zahrenna/mock/master/WhatsApp%20Image%202020-07-18%20at%2022.44.55.jpeg",
+    "productCategory" : "FOOD",
+    "contactNumber" : "https://wa.me/601111178945"
+  },
+  {
+    "userId": 1,
+    "id": 1,
+    "productName": "PAKSU BURGER CORNER",
+    "productPrice": "RM3 - RM6.50",
+    "productDescription": " Burger ayam / Daging rm3 \n Special Ayam/Daging rm3.50 \n Double Ayam/Daging rm4 \n Burger Chicken Grill/Chicken chop 💥 rm6.50 \n Delivery charge mengikut tempat \n Rumah Bertingkat hantar sampai atas extra charge rm1.50",
+    "productURL" : "https://raw.githubusercontent.com/zahrenna/mock/master/WhatsApp%20Image%202020-07-18%20at%2016.20.50.jpeg",
+    "productCategory" : "FOOD",
+    "contactNumber" : "https://wa.me/60187886220"
+  },
+  {
+    "userId": 1,
+    "id": 1,
+    "productName": "WESTERN set",
+    "productPrice": "RM5",
+    "productDescription": " Chicken Grill B.papper \n Meatball \n Chicken Chop \n Fich N Chip",
+    "productURL" : "https://raw.githubusercontent.com/zahrenna/mock/master/WhatsApp%20Image%202020-07-18%20at%2016.20.49.jpeg",
+    "productCategory" : "FOOD",
+    "contactNumber" : "https://wa.me/60187886220"
   }
 ]
